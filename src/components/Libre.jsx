7b2533e69395142b2543a3e0775aa6b5e3c9d2f5
alexx73 <tristanluna@yahoo.com.ar>
2024-12-14 23:54:08 -0300

    fix propiedades de los iconos svg

--- src/components/Libre.jsx
+++ src/components/Libre.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 function Libre(props) {
   return (
-    <div class="cursor-pointer" onClick={() => alert(props.cancha + "\n" + props.horas + " a " +  (props.horas+ 1) +  " horas")} >
-       <svg class="w-5 h-5 text-green-500 text-lg " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5"/>
-        </svg>                 
+    <div className="cursor-pointer" onClick={() => alert(props.cancha + "\n" + props.horas + " a " +  (props.horas+ 1) +  " horas")} >
+       <svg className="w-5 h-5 text-green-500 text-lg " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5"/>
+        </svg>   
+                      
     </div>
   )
 }
--- src/components/Ocupado.jsx
+++ src/components/Ocupado.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { TiTick } from "react-icons/ti";
 
 function Ocupado() {
   return (
     <div>
-        <svg class="w-4 h-4 text-red-500 text-lg pt-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+      {/* <TiTick className='w-10 h-10 text-red-500  '/> */}
+        <svg className="w-5 h-5 text-red-500 text-lg " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
         </svg>
     </div>
   )
